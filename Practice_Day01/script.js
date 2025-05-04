7773ad514e1d068134d175481813a749ae6e8d50
EvilxDhron <dhronyadavdhronyadav@gmail.com>
2025-05-04 20:52:36 +0530

    Question 5, 6 and 7 solved

--- Practice_Day01/script.js
+++ Practice_Day01/script.js
@@ -77,4 +77,60 @@ num1 > num2
   ? console.log("This is the largest number", num2)
   : console.log("This is the largest number", num3);  */
 
+// -------- Question - 5 🎀💎🎯
 
+/* Largest of Three Numbers – Take three numbers as input and
+print the largest number among them without using
+Math.max(). */
+
+// Solution 👇
+
+/* let year = Number(prompt("Enter the Year"));
+
+if ((year % 4 == 0 && year % 100 !== 0) || year % 400 === 0){
+  console.log(year + " is a leap year.");
+}else{
+  console.log(year + " is not a leap year.");
+}  */
+
+// -------- Question - 6 🎀💎🎯
+
+/* Simple Calculator – Ask the user for two numbers and an
+operator (+, -, *, /). Perform the operation and display the result. */
+
+// Solution 👇
+
+/* let num1 = Number(prompt("Enter the first number"));
+let num2 = Number(prompt("Enter the Second number"));
+let operate = prompt("Enter the operation you want to perform");
+
+if (operate == "+" || operate === "Plus") {
+  console.log(num1 + num2);
+} else if (operate == "-" || operate === "Minus") {
+  console.log(num1 - num2);
+} else if (operate == "/" || operate === "Divide") {
+  console.log(num1 / num2);
+} else if (operate == "*" || operate === "Multiply") {
+  console.log(num1 * num2);
+} else if (operate == "%" || operate === "Remainder") {
+  console.log(num1 % num2);
+} */
+
+// -------- Question - 7 🎀💎🎯
+
+/* Positive, Negative, or Zero – Take a number input and check if it
+is positive, negative, or zero. */
+
+// Solution 👇
+
+let user_input = Number(prompt("Enter a number"));
+
+if (user_input > 0) {
+  console.log("It's a Positive number.");
+} else if (user_input < 0) {
+  console.log("It's a Negative number");
+} else if (user_input == 0) {
+  console.log("It's only zero.");
+} else {
+  console.error("Not a number.");
+}
