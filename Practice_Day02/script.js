e11538f0dda845a40644df8bf5e70f9401e37c8d
EvilxDhron <dhronyadavdhronyadav@gmail.com>
2025-06-01 10:56:48 +0530

    Created a Mini Cart

--- Practice_Day02/script.js
+++ Practice_Day02/script.js
@@ -1,1 +1,14 @@
-/* Hello dosto kaise ho aap log */
+/* Mini Product Inventory System */
+
+let Cart = [
+  { Product: "Laptop", Quantity: 2, Price: 67000 },
+  { Product: "Smartphone", Quantity: 4, Price: 24000 },
+  { Product: "Tablet", Quantity: 5, Price: 26000 },
+  { Product: "Smartwatch", Quantity: 1, Price: 2400 },
+];
+
+const listitems = () => {
+    for(item of Cart){
+        console.log(`${item.Product} :- ${item.Quantity} Total-Price :- ${item.Price*item.Quantity}`);
+    }
+}
