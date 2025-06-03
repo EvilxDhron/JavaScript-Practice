24599d67e19b6f39f2b5f7f3c6fdbd06517ba05d
EvilxDhron <dhronyadavdhronyadav@gmail.com>
2025-06-03 09:27:19 +0530

    Dynamic Table completed

--- Dynamic_Table/script.js
+++ Dynamic_Table/script.js
@@ -1,1 +1,17 @@
 /* Dynamic HTML Table Project */
+
+const rows = 3;
+const cols = 4;
+const table = document.getElementById("myTable");
+table.style.color = "white";
+table.style.fontSize = "2rem";
+
+for(let i = 1; i <= rows; i++){
+    const tr = document.createElement("tr");
+    for(let c = 1; c <= cols; c++){
+        const td = document.createElement("td");
+        td.textContent = `Row ${i} Col ${c}`;
+        tr.appendChild(td);
+    }
+    table.appendChild(tr);
+}
