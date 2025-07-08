1f2a56d5cda51760212084b8294cf09abf387d09
EvilxDhron <dhronyadavdhronyadav@gmail.com>
2025-07-08 14:10:38 +0530

    51 Questions Solved

--- Revision/script.js
+++ Revision/script.js
@@ -395,3 +395,378 @@ if (digit >= 0 && digit <= 9) {
 } else {
   console.error("Only single digit numbers are allowed!");
 } */
+
+/* Q.26 Convert this if...else block into a ternary operator:
+
+if (age >= 18) {
+  message = "You can vote";
+} else {
+  message = "Too young";
+} */
+
+/* let age = Number(prompt("Enter your age here:"));
+
+age >= 18 ? console.log("You can Vote.") : console.log("Too young."); */
+
+/* Q.27 Write a function isEven that takes a number and returns true if it’s even, otherwise false. */
+
+/* let isEven = a => a % 2 === 0;
+let num = Number(prompt("Enter a number here:"));
+alert(isEven(num)); */
+
+/* Q.28 Write a function that accepts an array of student objects (each with name and marks) and returns the names of students who scored above 80. */
+
+/* let printer = (a) => {
+  for (let stud of a) {
+    if (stud.marks >= 80) {
+      console.log(`${stud.name} - ${stud.marks}`);
+    }
+  }
+}; */
+
+/* let printer = a => {
+  return a.filter(s => s.marks >= 80).map(s => s.name);
+}
+
+let arr = [
+  { name: "Karan", marks: 95 },
+  { name: "Arjun", marks: 85 },
+  { name: "Bheem", marks: 90 },
+  { name: "Duryodhan", marks: 79 },
+  { name: "Akshay", marks: 65 },
+  { name: "Amar", marks: 80 },
+  { name: "Radhika", marks: 99 },
+]; */
+
+/* Q.29 You have an object:
+
+let user = {
+  name: "Alice",
+  purchases: ["Book", "Pen"]
+};
+Add a function that adds a new item to purchases. */
+
+/* let user = {
+  name: "Akshay",
+  purchases: ["Book", "Pen"],
+  addtopurchase: function (item) {
+    this.purchases.push(item);
+  },
+};
+
+user.addtopurchase("Notebook");
+console.log(user); */
+
+// let adds = (item) => user.purchases.push(item);
+
+// adds("Rubber");
+// console.log(user);
+
+/* Q.30 Make a function that calculates the average age of an array of people:
+
+[{name: "A", age: 20}, {name: "B", age: 30}, {name: "C", age: 25}] */
+
+/* let Peoples = [
+  { name: "Amit", age: 67 },
+  { name: "Sita", age: 82 },
+  { name: "Ram", age: 91 },
+  { name: "Geeta", age: 76 },
+  { name: "Lakshman", age: 88 },
+]; */
+
+/* let avgCalt = (peo) => {
+  let sum = 0;
+  for (let item of peo) {
+    sum += item.age;
+  }
+  return sum / peo.length;
+}; */
+
+/* let avgCalt = (peo) => {
+  let total = peo.reduce((acc, key) => (acc += key.age), 0);
+  return total / peo.length;
+};
+
+console.log(avgCalt(Peoples)); */
+
+/* Q.31 Loop through an object and print all key-value pairs. */
+
+/* const person = {
+  name: "Alex",
+  age: 25,
+  city: "London",
+  isEmployed: true,
+  skills: ["JavaScript", "HTML", "CSS"],
+};
+
+for (let key in person) {
+  console.log(`${key} -> ${person[key]}`);
+} */
+
+/* Q.32 Given:
+
+let message = "Hello JavaScript World!";
+Extract the word "JavaScript" from the string. */
+
+/* let message = "Hello JavaScript World!";
+
+let word = message.split(" ").find((w)=> w.includes("JavaScript"));
+console.log(word); */
+
+/* Q.33 Convert the string "hello world" into "Hello World". */
+
+/* let message = "hello world";
+
+let words = message.split(" ");
+let capitalizer = words.map((word) => {
+  return word[0].toUpperCase() + word.slice(1);
+});
+
+let result = capitalizer.join(" ");
+console.log(result); */
+
+/* Q.34 Check if the string "I love JS" contains "love". */
+
+/* let feeling = "I love JS";
+let finder = feeling.includes("love");
+console.log(finder); */
+
+/* Q.35 Reverse a string: "Code" → "edoC" */
+
+/* let word = "Code";
+let final = word.split("").reverse().join("");
+console.log(final) */
+
+/* Q.36 Count how many times a character appears in a string.
+Example: 'programming', count 'g'. */
+
+/* let word = "programming";
+let counter = {};
+
+for (let letter of word) {
+  if (counter[letter] === undefined) {
+    counter[letter] = 1;
+  } else {
+    counter[letter] += 1;
+  }
+}
+
+console.log(counter); */
+
+/* Q.37 Count how many times a character appears in a string.
+Example: 'programming', count 'g'. */
+
+/* let word = "programming";
+
+let final = word.split("").filter((w) => w === "g").length;
+console.log(final); */
+
+/* Q.38 Given:
+let nums = [1, 2, 3, 4, 5];
+Return a new array with each number squared. */
+
+/* let nums = [1, 2, 3, 4, 5];
+let final = nums.map((n) => n * n);
+console.log(final); */
+
+/* Q.39 Remove all even numbers from the array.  */
+
+/* let num = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+let final = num.filter((L) => L % 2 != 0);
+console.log(final); */
+
+/* Q.40 Flatten a 2D array:
+[[1, 2], [3, 4], [5]] */
+
+/* let arr = [[1, 2], [3, 4], [5]];
+let flatten = arr.flat(); */
+
+/* let arr = [[1, 2], [3, 4], [5]];
+let flatten =[];
+
+for(let ar of arr){
+  for(let item of ar){
+    flatten.push(item);
+  }
+}
+
+console.log(flatten); */
+
+/* Q.41 Sort the array [3, 1, 4, 2] in descending order. */
+
+/* let arr = [3, 1, 4, 2];
+
+console.log(arr.sort((a, b)=> b-a)); */
+
+/* Q.42 Find the maximum value in an array. */
+
+/* let arr = [32, 45, 56, 99, 32, 23, 78, 93, 98];
+let max = 0;
+for(let ar of arr){
+  if(max < ar){
+    max = ar;
+  }
+}
+
+console.log(max); */
+
+/* Q.43 Combine two arrays without duplicates:
+
+[1, 2, 3] + [3, 4, 5] */
+
+/* let arr1 = [1, 2, 3];
+let arr2 = [3, 4, 5];
+
+let final = new Set([...arr1, ...arr2]);
+console.log([...final]); */
+
+/* Q.44 Add a method introduce() to this object which returns: */
+// “Hi, I’m Avi and I love coding.”
+
+/* let obj = {
+  name: "Avi",
+  class: "3rd",
+  introduce: function(){
+    return `Hi I'm ${this.name} and I love coding.`
+  }
+}
+console.log(obj.introduce()); */
+
+/* Q.45 You have this data:
+
+let users = [
+  { name: "Avi", age: 25 },
+  { name: "Raj", age: 17 },
+  { name: "Sim", age: 30 }
+];
+Write a function that returns users above 18. */
+
+/* let users = [
+  { name: "Avi", age: 25 },
+  { name: "Raj", age: 17 },
+  { name: "Sim", age: 30 },
+];
+
+let toppers = users.filter((obj)=> obj.age > 18);
+console.log(toppers[0].name, toppers[1].name); */
+
+/* Q.46 Count how many users have names starting with "A". */
+
+/* let users = [
+  { name: "Avi", age: 25 },
+  { name: "Raj", age: 17 },
+  { name: "Sim", age: 30 },
+  { name: "John", age: 22 },
+  { name: "Emma", age: 19 },
+  { name: "Liam", age: 28 },
+  { name: "Olivia", age: 35 },
+  { name: "Noah", age: 16 },
+  { name: "Ava", age: 27 },
+  { name: "William", age: 42 },
+  { name: "Sophia", age: 31 },
+  { name: "James", age: 24 },
+  { name: "Isabella", age: 18 },
+  { name: "Benjamin", age: 29 },
+  { name: "Mia", age: 33 },
+  { name: "Lucas", age: 20 },
+  { name: "Charlotte", age: 26 },
+  { name: "Henry", age: 38 },
+  { name: "Amelia", age: 21 },
+  { name: "Alexander", age: 45 },
+  { name: "Harper", age: 17 },
+  { name: "Michael", age: 32 },
+  { name: "Evelyn", age: 23 },
+  { name: "Daniel", age: 40 },
+  { name: "Abigail", age: 15 },
+  { name: "Matthew", age: 36 },
+  { name: "Emily", age: 28 },
+  { name: "Jackson", age: 19 },
+  { name: "Elizabeth", age: 34 },
+  { name: "David", age: 27 },
+  { name: "Sofia", age: 22 },
+  { name: "Joseph", age: 39 },
+  { name: "Avery", age: 16 },
+  { name: "Samuel", age: 31 },
+  { name: "Ella", age: 25 },
+  { name: "Sebastian", age: 43 },
+  { name: "Scarlett", age: 20 },
+  { name: "Jack", age: 37 },
+  { name: "Grace", age: 18 },
+  { name: "Owen", age: 29 },
+  { name: "Chloe", age: 24 },
+  { name: "Dylan", age: 41 },
+  { name: "Victoria", age: 30 },
+  { name: "Luke", age: 21 },
+  { name: "Riley", age: 26 },
+  { name: "Gabriel", age: 35 },
+  { name: "Zoey", age: 17 },
+  { name: "Andrew", age: 44 },
+  { name: "Lily", age: 19 },
+  { name: "Julian", age: 32 },
+  { name: "Hannah", age: 23 },
+];
+
+let count = users.filter((user)=> user.name.startsWith("A"));
+let final = [];
+for(let user of count){
+  final.push(user.name);
+}
+console.log(final); */
+
+// Q.47 Write a function that returns the factorial of a number.
+
+/* let factorial = n => {
+  let result = 1;
+  for(let i = 2; i <= n; i++){
+    result *= i;
+  }
+  return result;
+}
+
+console.log(factorial(20)); */
+
+/* let factorial = (n) => {
+  if (n === 0 || n === 1) {
+    return 1;
+  }
+  return n * factorial(n - 1);
+};
+
+console.log(factorial(20)); */
+
+// Q.48 Create a function that takes an array of numbers and returns only the even ones.
+
+/* let evenM = (arr) => arr.filter((a) => a % 2 === 0);
+console.log(evenM([1, 2, 3, 4, 5, 6, 7, 8, 9, 10])); */
+
+// Q.49 Write a function that accepts a string and returns true if it’s a palindrome.
+
+/* let paliChecker = (str) =>
+  str.split("").reverse().join("") === str ? "Palindrome" : "not a palindrome";
+console.log(paliChecker("Palak")); */
+
+// Q.50 Write a function that calculates average from any array of numbers.
+
+/* let avg = arr => arr.reduce((acc, key)=> {return acc + key},0) / arr.length;
+console.log(avg([1,2,3,4,5])); */
+
+/* Q.51 Make a function that takes a person object and returns:
+
+"Avi is 25 years old and likes coding, gaming" */
+
+/* let introducer = person => `${person.name} is ${person.age} years old and likes ${person.hobbies[0]}, ${person.hobbies[1]}.`;
+console.log(introducer({ name: "Luke", age: 21, hobbies: ["Chess", "Reading"] })); */
+
+/* const myPromise = new Promise((resolve, reject) => {
+  // Asynchronous work here...
+  setTimeout(() => {
+    const success = true; // or false to simulate error
+    if (success) {
+      resolve("Data loaded successfully");
+    } else {
+      reject("Something went wrong");
+    }
+  }, 1500);
+}); */
+
+
